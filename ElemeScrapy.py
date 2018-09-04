46c0d9aa383d62f0ea5b4ce83926e3884fa2c210
Zhenzhong Mao <dkrose.random@gmail.com>
2018-09-04 18:24:35 +0800

    20180904

--- ElemeScrapy.py
+++ ElemeScrapy.py
@@ -15,6 +15,10 @@ headers = {
 
 chromedriverpath = r"C:\Users\kmao009\Documents\chromedriver.exe"
 
+mainUrl = "https://www.ele.me/restapi/shopping/restaurants?extras%5B%5D=activities&geohash=wtw3985r58m2&latitude={}&longitude={}&terminal=web&offset=0&limit=25"
+
+menuUrl = "https://www.ele.me/restapi/shopping/v2/menu?restaurant_id={}&terminal=web"
+
 def simple_get(url,headers):
 
     browser = webdriver.Chrome(chromedriverpath)
@@ -29,17 +33,48 @@ def is_good_respoonse(resp):
 def log_error(e):
     print(e)
 
-raw_html = simple_get('https://www.ele.me/restapi/shopping/v2/menu?restaurant_id=19502&terminal=web',headers)
+def getAllrestaurantByGeo(latitude,longitude,headers):
+    url = mainUrl.format(latitude,longitude)
+    restaurants_html = simple_get(url,headers)
+    restaurants_array = json.loads(restaurants_html.text)
+    restaurantIds = []
+    for restaurant in restaurants_array:
+        print(restaurant['id'])
+        # restaurant_details = restaurant['folding_restaurants']
+        # for detail in restaurant_details:
+        #     print(detail['name'])
+        restaurantIds.append(restaurant['id'])
+    return restaurantIds
 
-json_array = json.loads(raw_html.text)
 
 
-for info in json_array:
-    foodarray = info['foods']
-    for foodinfo in foodarray:
-        specfoods = foodinfo['specfoods']
-        for specfood in specfoods:
-            fooddetail = str(specfood['name']) + " : " + str(specfood['price']) + ", " + str(specfood['specs'])
-            print(fooddetail)
+# raw_html = simple_get('https://www.ele.me/restapi/shopping/v2/menu?restaurant_id=165991466&terminal=web',headers)
 
+# json_array = json.loads(raw_html.text)
+
+
+# for info in json_array:
+#     foodarray = info['foods']
+#     for foodinfo in foodarray:
+#         specfoods = foodinfo['specfoods']
+#         for specfood in specfoods:
+#             fooddetail = str(specfood['name']) + " : " + str(specfood['price']) + ", " + str(specfood['specs'])
+#             print(fooddetail)
+
+def listAllMenuByrestaurant(latitude,longitude,headers):
+    ids = getAllrestaurantByGeo(latitude,longitude,headers)
+    for id in ids:
+        tmpUrl = menuUrl.format(id)
+        menuhtml = simple_get(tmpUrl,headers)
+        menu_array = json.loads(menuhtml.text)
+        for info in menu_array:
+            foodarray = info['foods']
+            for foodinfo in foodarray:
+                specfoods = foodinfo['specfoods']
+                for specfood in specfoods:
+                    fooddetail = str(specfood['name']) + " : " + str(specfood['price']) + ", " + str(specfood['specs'])
+                    print(fooddetail)
+
+
+listAllMenuByrestaurant('31.202375','121.359601',headers)
 
